7b791e333eca19e95efc6eb1b06e6771ff88952b
Kronyx <theweaponshop@outlook.com>
2015-08-17 20:52:29 -0400

    Added a couple rebel weapons
    Added some stuff to the gang hideouts.

--- MPMissions/AL4.Altis/Config_Shops.hpp
+++ MPMissions/AL4.Altis/Config_Shops.hpp
@@ -37,7 +37,7 @@ class WeaponShops {
             { "30Rnd_9x21_Mag", "", 200 },
             { "20Rnd_556x45_UW_mag", "", 125 },
             { "30Rnd_556x45_Stanag", "", 300 },
-            { "10Rnd_762x51_Mag", "", 500 },
+            { "10Rnd_762x54_Mag", "", 500 },
             { "30Rnd_65x39_caseless_green", "", 275 }
         };
     };
@@ -49,15 +49,31 @@ class WeaponShops {
         msg = "";
         items[] = {
             { "hgun_Rook40_F", "", 1500 },
-            { "hgun_Pistol_heavy_02_F", "", 2500 },
-            { "hgun_ACPC2_F", "", 4500 },
-            { "hgun_PDW2000_F", "", 9500 },
-            { "optic_ACO_grn_smg", "", 950 },
-            { "V_Rangemaster_belt", "", 1900 },
-            { "16Rnd_9x21_Mag", "", 25 },
-            { "9Rnd_45ACP_Mag", "", 45 },
-            { "6Rnd_45ACP_Cylinder", "", 50 },
-            { "30Rnd_9x21_Mag", "", 75 }
+			{ "30Rnd_9x21_Mag", "", 100 },
+			{ "16Rnd_9x21_Mag", "", 60 },
+			{ "hgun_Pistol_heavy_01_snds_F", "", 2000 },
+			{ "11Rnd_45ACP_Mag", "", 70 },
+			{ "arifle_Katiba_F", "", 30000 },
+			{ "arifle_Katiba_ARCO_pointer_snds_F", "", 55000 },
+			{ "30Rnd_65x39_caseless_green", "", 100 },
+			{ "arifle_MXC_F", "", 40000 },
+			{ "30Rnd_65x39_caseless_mag", "", 100 },
+			{ "srifle_DMR_02_F", "", 50000 },
+			{ "10Rnd_338_Mag", "", 125 },
+			{ "MMG_01_hex_F", "", 75000 },
+			{ "150Rnd_93x64_Mag", "", 250 },
+			{ "srifle_GM6_F", "", 250000 },
+			{ "5Rnd_127x108_Mag", "", 500 },
+			{ "5Rnd_127x108_APDS_Mag", "", 1000 },
+			{ "launch_RPG32_F", "", 100000 },
+			{ "RPG32_F", "", 25000 },
+			{ "RPG32_HE_F", "", 50000 },
+			{ "optic_LRPS", "", 15000 },
+			{ "optic_DMS", "", 12500 },			
+			{ "optic_Aco", "", 2000 },			
+			{ "optic_Arco", "", 2500 },			
+			{ "optic_Hamr", "", 1000 }						
+
         };
     };
     //Basic Shops
